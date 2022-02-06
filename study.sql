1601d5b274bf1638afaa7a8cf41da33d50ef838a
片江友哉 <tomoya.katae@gmail.com>
2022-02-06 21:07:05 +0900

    004

--- study.sql
+++ study.sql
@@ -4,6 +4,7 @@ SELECT * EXCEPT (revenue), revenue*1.1 AS 'revenue_with_tax'
 FROM sample.sales
 ORDER BY order_id ASC
 LIMIT 3
+
 --002
 SELECT order_id
 , quantity
@@ -14,10 +15,21 @@ SELECT order_id
 FROM sample.sales
 ORDER BY new_revenue DESC
 LIMIT 3
+
 -- 003
 SELECT *
 FROM sample.customers
 WHERE birthday IS NOT NULL
 AND id_premium = TRUE
 ORDER BY birthday DESC, register_date ASC
+LIMIT 3
+
+--004
+SELECT *
+FROM sample.customers
+WHERE (id_premium IS TRUE OR birthday BETWEEN '1970-01-01' AND '1979-12-31')
+AND name LIKE '%美'
+AND gender = 2
+-- 女性指定忘れてた
+ORDER BY birthday ASC
 LIMIT 3
