0be19688a25fa30f3cc5c0fa58e44d716ee333ad
片江友哉 <tomoya.katae@gmail.com>
2022-02-06 20:41:24 +0900

    002

--- study.sql
+++ study.sql
@@ -4,3 +4,13 @@ SELECT * EXCEPT (revenue), revenue*1.1 AS 'revenue_with_tax'
 FROM sample.sales
 ORDER BY order_id ASC
 LIMIT 3
+--002
+SELECT order_id
+, quantity
+, revenue
+, quantity+1 AS new_qty
+, (quantity+1)*(revenue/quantity) AS new_revenue
+-- 単価を求めるところができてなかった
+FROM sample.sales
+ORDER BY new_revenue DESC
+LIMIT 3
